aa609e5906db526bcb343c295274925fd5ffa0c8
Dhruv kathiriya <150034575+Dhruv-Kathiriya@users.noreply.github.com>
2024-04-16 19:23:47 +0530

    Update 2.dart

--- 2.dart
+++ 2.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 void main() {
   List<String> statesList = [];
   for (int i = 1; i <= 28; i++) {
-    stdout.write("Enter State $i Name : ");
-    String name = stdin.readLineSync()!;
+    stdout.write("Enter State Name : ");
+    String name = stdin.readLineSync() ?? "0";
     statesList.add(name);
   }
   statesList.forEach((element) => print('$element'));
